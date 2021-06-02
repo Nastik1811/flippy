868a6d2551da228d252d09b97206d322a76af7d1
Nastik1811 <lugovskaya.1999@mail.ru>
2021-06-02 11:22:52 +0300

    Implement cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions"
 import admin = require('firebase-admin');
+import {getInterval, getStatus} from './utils'
 
 admin.initializeApp(functions.config().firebase)
 
@@ -15,15 +16,53 @@ export const createDefaultCollection = functions.auth.user().onCreate(
         })
     }
 )
+    
 
 export const updateCardProgress = 
 functions.firestore.document('users/{userId}/reviews/{reviewId}').onCreate(
     (snap, context) => {
-       
+       const reviewData = snap.data()
+       const nextIntervalInMs = getInterval(reviewData.cardStatus, reviewData.ease, reviewData.previousInterval, reviewData.delay)
+       const status = getStatus(reviewData.status, reviewData.ease, nextIntervalInMs)
+   
+       const lastReview = new Date()
+       const scheduledReview = new Date().setMilliseconds(nextIntervalInMs)
+
+       return admin.firestore().collection('users')
+       .doc(context.params.userId).collection('cards')
+       .doc(reviewData.cardId)
+       .update({
+            lastReview,
+            scheduledReview,
+            status
+        })
     }
 )
 
+export const checkCardsAndNotifyUser = functions.pubsub.schedule('every 24 hours').onRun(() => {
+    return admin.firestore().collection('users').get().then(data => data.docs.forEach(async user => {
+    
+    const cardsToRepeat = (await admin.firestore().collection('users')
+        .doc(user.id).collection('cards')
+        .where("scheduledReview", "<=", new Date())
+        .get()).size
 
-// export const cardReview = functions.firestore.document('/users/{userId}/reviews/{reviewId}').onCreate(
-//     () => {}
-// )
+    const tokens: string[] = user.data().notificationTokens
+    if(!tokens){
+        return null;
+    }
+    const message: admin.messaging.MessagingPayload = {
+        notification: {
+            title: `Hey, ${user.data().userName || 'User'}!`,
+            body: `Don't forget review your cards! There are ${cardsToRepeat} cards waiting for you!`
+        }
+    }
+    return admin.messaging().sendToDevice(tokens, message).then((response) => {
+        console.log('Successfully sent message:', response);
+        })
+        .catch((error) => {
+            console.log('Error sending message:', error);
+        })
+}))
+    
+})
